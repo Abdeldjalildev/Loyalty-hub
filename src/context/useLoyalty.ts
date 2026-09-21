003843520d28e15254ca04d77a3dae322b55a12d
KHALFA abdeldjalil <abdeldjalil.khalfa@enssmal.edu.dz>
2026-09-21 19:04:40 +0100

    refactor: separate context definitions from providers and hooks

---
+++ src/context/useLoyalty.ts
@@ -0,0 +1,8 @@
+import { useContext } from 'react';
+import { LoyaltyContext } from './LoyaltyContext';
+
+export const useLoyalty = () => {
+  const context = useContext(LoyaltyContext);
+  if (!context) throw new Error('useLoyalty must be used within a LoyaltyProvider');
+  return context;
+};
